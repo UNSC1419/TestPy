af1d9df04ace27af5a1daf9ece3284325f3b33cb
UNSC1419 <ahtamsadik@gmail.com>
2020-11-14 04:38:35 +0900

    Default Changelist

--- ah_18_九九乘法表.py
+++ ah_18_九九乘法表.py
@@ -2,7 +2,7 @@ row = 1
 col = 1
 while row <= 9:
     while col <= row:
-        print("%-2d * %-2d = %-2d" % (col, row, row * col), end=" ")
+        print("%d*%d=%d\t" % (col, row, row * col), end="")
         col += 1
     print("")
     col = 1
