2da27286595af0dd5fa2eca6378e361a6608905b
UNSC1419 <ahtamsadik@gmail.com>
2020-11-14 19:23:53 +0900

    Default Changelist

--- ah_19_函数.py
+++ ah_19_函数.py
@@ -3,4 +3,29 @@ def say_hello():
     print("你好啊！")
 
 
-say_hello()
+# say_hello()
+
+
+def sum_2_num(num1, num2):
+    """两个数字求和函数,参数1（int），参数2（int）"""
+    result = num1 + num2
+    return result
+
+
+# print(sum_2_num(1, 2))
+
+
+def 打印横线(字符, 列数, 行数):
+    """打印多行分割线
+
+    :param 字符:分割线使用的字符
+    :param 列数:分割线的列数
+    :param 行数:分割线的行数
+    """
+    row = 0
+    while row < 行数:
+        print(字符 * 列数)
+        row += 1
+
+
+打印横线("!", 10, 5)
