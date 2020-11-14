1f47400b7406b0e0a447e7caf76a395a72596b53
UNSC1419 <ahtamsadik@gmail.com>
2020-11-14 15:19:12 +0900

    Default Changelist

--- ah_18_九九乘法表.py
+++ ah_18_九九乘法表.py
@@ -3,16 +3,16 @@ row = 1
 col = 1
 # 行数循环
 while row <= 9:
-    #列数循环
+    # 列数循环
     while col <= row:
         # 打印例子2*3=6（对齐制表符）,取消换行
         print("%d*%d=%d" % (col, row, row * col), end="\t")
         # 列计数器
         col += 1
-    #打印换行
+    # 打印换行
     print("")
-    #初始化列计数器
+    # 初始化列计数器
     col = 1
-    #行计数器
+    # 行计数器
     row += 1
 0
